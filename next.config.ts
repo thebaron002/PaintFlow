dad42ee5e68fb4ed058859762583c444ee907957
Henrique Gonçalves <henriquegoncal@gmail.com>
2025-10-27 04:32:21 +0000

    O problema que você observa no Safari/iOS não é um bug do código, mas um

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -29,6 +30,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com/__/auth/:path*`,
+      },
+    ];
   },
 };
 
